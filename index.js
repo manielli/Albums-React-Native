26b958135e92283716482a843617e2315949cef5
Mani Jafari <manielli@gmail.com>
2019-02-27 20:01:20 -0800

    Added a Header component

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
 
-const App = () => {
-    return (
-        <Text>Some Text</Text>
-    );
-};
+const App = () => (
+    <Text style={{ flex: 1, backgroundColor: 'white', fontSize: 20 }} >Some Text</Text>
+);
 
 AppRegistry.registerComponent('albums', () => App);
