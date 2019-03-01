4bc2f69f84bc6bb1ca9aaca2df923f2142a6ab90
Mani Jafari <manielli@gmail.com>
2019-02-28 17:23:07 -0800

    Changed the rootView background default color of the app to be white, added a little bit of styling in Header component

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/Header';
 
 const App = () => (
-    <Text style={{ flex: 1, backgroundColor: 'white', fontSize: 20 }} >Some Text</Text>
+    <Header />
 );
 
 AppRegistry.registerComponent('albums', () => App);
+
